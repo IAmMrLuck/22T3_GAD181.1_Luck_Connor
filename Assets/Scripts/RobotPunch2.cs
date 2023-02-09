bac5606e262ef7ad7ddf5d9d353d68a8fed08824
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-09 18:32:25 +1100

    Arms punched, and now they don't
    I got both the left and right arms of the red robot to punch
    but now they don't work
    
    I'm committing this, and restarting to hopefully fix the problem

--- Assets/Scripts/RobotPunch2.cs
+++ Assets/Scripts/RobotPunch2.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 
 namespace ConaLuk
 {
-    public class RobotPunch : MonoBehaviour
+    public class RobotPunch2 : MonoBehaviour
     {
-        [SerializeField] private 
+        [SerializeField]
+        private
 
         // Update is called once per frame
         void Update()
         {
             // player presses left arrow and the arm rotates
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 transform.Rotate(0, 0, -90);
             }
 
             //when the player lets go of the left arrow, the arm returns to it's starting position
-            if(Input.GetKeyUp(KeyCode.LeftArrow))
+            if (Input.GetKeyUp(KeyCode.RightArrow))
             {
                 transform.Rotate(0, 0, 90);
             }

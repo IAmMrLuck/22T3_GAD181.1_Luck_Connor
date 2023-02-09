f9bad7d99e96986fbba8e1fda4e1734f3177e667
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-09 21:10:51 +1100

    Added a Timer
    I've added a timer which counts down from a specified number to zero and then stops.
    No further functionality yet.

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] private float TimeLeft;
+    [SerializeField] private bool TimerOn = false;
+    [SerializeField] private TMP_Text TimerText;
+
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+        }
+    }
+
+    void updateTimer (float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+
+
+    }
+}
